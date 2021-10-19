61c4cfb825ea45b4a589ac952d769e7de40fada9
Daejv <72608569+Daejv@users.noreply.github.com>
2021-10-19 12:59:04 +0200

    Add files via upload

---
+++ wycieczki.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wycieczki i urlopy</title>
+    <link rel="stylesheet" href="styl3.css">
+</head>
+<body>
+    <header>
+        <h1>BIURO PODRÓŻY</h1>
+
+    </header>
+        <div id="lewy">
+            <h2>KONTAKT</h2>
+            <a href="mailto:biuro@wycieczki.pl">napisz do nas</a>
+            <p>telefon: 555666777</p>
+        </div>
+        
+        <div id="srodkowy">
+            <h2>GALERIA</h2>
+            <img src="obraz1.jpeg">
+            <img src="obraz2.jpg">
+            <img src="obraz3.jpg">
+            <img src="obraz4.jpeg">
+            <img src="obraz5.jpg">
+            <img src="obraz6.jpg">
+            <img src="obraz7.jpg">
+            <img src="obraz8.jpg">
+            <img src="obraz9.jpg">
+            
+            <?php
+            //skrypt1
+            ?>
+        </div>
+        
+        <div id="prawy">
+            <h2>PROMOCJE</h2>
+            <table>
+                <tr>
+                    <td>Jesień</td>
+                    <td>Grupa 4+</td>
+                    <td>Grupa 10+</td>
+                </tr>
+                <tr>
+                    <td>5%</td>
+                    <td>10%</td>
+                    <td>15%</td>
+                </tr>
+            </table>
+        </div>  
+
+        <div id="dane">
+            <h2>LISTA WYCIECZEK</h2>
+            <?php
+            //skrypt2
+            ?>
+        </div> 
+        
+    <footer>
+<p>Strone wykonał: 00000000000</p>
+    </footer>
+</body>
+</html>
